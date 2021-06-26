b1099d60402e0932e3532098a5ce456e6147b81b
normndakbr <normnd.akbr2@gmail.com>
2021-06-26 07:15:21 +0700

    using state, and looping

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import react from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends react.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            username: "Akbar",
+            userDetail: {
+                id: 3,
+                name: "Pikachu"
+            },
+            users: [
+                {
+                    id: 1,
+                    name: "Squirtle"
+                },
+                {
+                    id: 2,
+                    name: "Charmender"
+                },
+                {
+                    id: 4,
+                    name: "Totodile"
+                }
+            ]
+        }
+    }
+
+    render() {
+
+        const {username, userDetail, users} = this.state
+        return (
+            <>
+                <h1> Hello World </h1>
+                <h3> This is subtitle </h3>
+                <div>{this.state.username}</div>
+                <div>{JSON.stringify(userDetail)}</div>
+                {/* <div>{JSON.stringify(users)}</div> */}
+                <ul>
+                    {
+                        users.map(user => {
+                            return<li>{user.name}</li>
+                        })
+                    }
+                </ul>
+            </>
+        )
+    }
 }
 
 export default App;
