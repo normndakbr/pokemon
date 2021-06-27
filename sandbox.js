abaabbd72d0963ca2c33e75e976ee7f1cd865716
normndakbr <normnd.akbr2@gmail.com>
2021-06-28 04:14:09 +0700

    first route initialization

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,29 @@
 const { createStore } = require('redux');
 
+//state initialization
 const initialState = { 
-  id: 1,
+  weight: 1,
   name: "Bulbasaur"
 }
 
-function reducer(state, action) {
-  if(state === undefined) {
-    return initialState
-  } else {
-
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case "INCREMENT":
+      let clonedState = {...state}
+      clonedState.weight = state.weight+1
+      return clonedState
   }
 }
 
+//Store initialization
 const store = createStore(reducer)
 
-console.log(store.getState(), "This is the first store")
+console.log(store.getState(), "This is first state")
+store.dispatch({type: "INCREMENT"})
+console.log(store.getState(), "Updated State")
+store.dispatch({type: "INCREMENT"})
+console.log(store.getState(), "Updated State")
+store.dispatch({type: "INCREMENT"})
+console.log(store.getState(), "Updated State")
+store.dispatch({type: "INCREMENT"})
+console.log(store.getState(), "Updated State")
--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Pokemon from './components/Pokemon'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <div>
-        <Pokemon />
+    <>
+      <div className="App">
+        <h1>Hello World!</h1>
+        <Switch>
+          <Route>
+
+          </Route>
+        </Switch>
       </div>
-    </div>
-  );  
+    </>
+  );
 }
 
 export default App;
