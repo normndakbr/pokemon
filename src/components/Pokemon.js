64f4e78c1cdee1090245161e0c8230d8fe4ea3a7
normndakbr <normnd.akbr2@gmail.com>
2021-06-27 13:55:55 +0700

    added comment

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -14,7 +14,10 @@ function useFetch() {
       })
   }, [])
 
+  //this will return data as an array
   // return [pokeData, setPokedata]
+
+  //this will return data as an object
   return {
     data: pokeData,
     setData: setPokedata
@@ -22,13 +25,17 @@ function useFetch() {
 }
 
 export default function Pokemon(props) {
+  //handle fetch data as an array
   // const [data, setData] = useFetch([])
-  const {data, setData} = useFetch([])
+
+  //handle fetch data as an object
+  //you can rename it by assign new name for data using data: pokemon
+  const {data: pokemons, setData} = useFetch([])
 
   return (
     <ul>
       {
-        data.map(pokemon => <li key={pokemon.id}>{pokemon.name}</li>)
+        pokemons.map(pokemon => <li key={pokemon.id}>{pokemon.name}</li>)
       }
     </ul>
   );
