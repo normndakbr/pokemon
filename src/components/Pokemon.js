ac9dd795916d8d982e2b2396772bfd4d478548ff
normndakbr <normnd.akbr2@gmail.com>
2021-06-27 11:49:36 +0700

    fetch data from poke-API by useEffect

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export default function Pokemon(props) {
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20")
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <ul>
       <li>Pokemon</li>
