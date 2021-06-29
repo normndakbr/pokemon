59f8a71acfd6a7375209a7d0ca481e2262f779cf
normndakbr <normnd.akbr2@gmail.com>
2021-06-29 13:36:21 +0700

    test layouting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,17 +13,29 @@ export default function Home() {
   console.log(pokemonList, "This is your state")
 
   return (
-    <>
-      <h1>Hello, welcome to Pokemon World!</h1>
-      <h3>Gotta Catch 'em all!</h3>
-      <pre> {JSON.stringify(pokemonList, null, 2)} </pre>
-      {/* <ul>
-        {
-          listPokemon.map(pokemon => {
-            return <li key={pokemon.id}>{pokemon.name}</li>
-          })
-        }
-      </ul> */}
-    </>
+    <div class="container columns">
+      {/* <h1>Hello, welcome to Pokemon World!</h1> */}
+      {/* <h3>Gotta Catch 'em all!</h3> */}
+      {/* <pre> {JSON.stringify(pokemonList, null, 2)} </pre> */}
+      {/* <ul> */}
+      {
+
+        pokemonList.map(pokemon => {
+          return (
+            <div class="column is-one-fifth">
+              <div class="card">
+                <div class="card-header">
+                  <div class="card-header-title">
+                    <p>{pokemon.name}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            // <li key={pokemon.id}>{pokemon.name}</li>
+          )
+        })
+      }
+      {/* </ul> */}
+    </div>
   )
 }
