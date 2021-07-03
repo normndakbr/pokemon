2c4d8157c47bcdf124768de029e929a3892aa242
normndakbr <normnd.akbr2@gmail.com>
2021-07-03 17:50:15 +0700

    fixed release (delete) my pokemon feature

--- src/pages/MyPokemon.js
+++ src/pages/MyPokemon.js
@@ -6,18 +6,31 @@ import Navi from '../components/Navi';
 export default function MyPokemon() {
   let [myPokemonList, setMyPokemonList] = useState([])
 
+  function removePokemon(id) {
+    let previousData = myPokemons()
+    let x = previousData.findIndex((element, i) => element.id === id)
+    let result = previousData.filter(pokemon => pokemon.id !== x)
+    myPokemons(result)
+    setMyPokemonList(result)
+  }
+
   useEffect(() => {
-    setMyPokemonList(myPokemons())
+    console.log("masuk sini")
+    let pokemonData = myPokemons()
+    setTimeout(() => {
+      setMyPokemonList(pokemonData)
+    }, 1000)
+    console.log(pokemonData)
   }, [myPokemonList])
 
   return (
-    <div style={{backgroundColor: '#32373e'}} className="container is-fluid is-fullheight">
-      <Navi/>
+    <div style={{ backgroundColor: '#32373e' }} className="container is-fluid is-fullheight">
+      <Navi />
       <div className="columns is-multiline is-mobile is-5 is-variable is-centered">
         {
           myPokemonList.map(pokemonData => {
             return (
-              <MyPokeCard key={pokemonData.id} detailOfPokemon={pokemonData} setMyPokemonList={setMyPokemonList} />
+              <MyPokeCard key={pokemonData.id} detailOfPokemon={pokemonData} setMyPokemonList={setMyPokemonList} removePokemon={removePokemon} />
             )
           })
         }
